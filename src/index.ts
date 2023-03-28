059f59a67e9c67975e15130717628100db5e5dad
LeandroGazoli <leandro.gazoli@outlook.com>
2023-03-28 19:21:46 -0300

    Adicionando o typescripte incrementando a foma de desenvolvimento

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('teste');
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.use('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: `bar` });
+});
+
+app.listen(1515, () => {
+  console.log(`servidor online`);
+});
